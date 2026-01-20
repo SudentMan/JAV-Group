6b8c82ce0dc71b0d827298ed8e802908a9b23269
Tht-worada <thitibell123@gmail.com>
2026-01-20 22:13:03 +0700

    add () on output by Thitiworada
    ลืม แก้ เติม () ที่ output ฉบับสุดท้ายเถอะขอร้อง

--- project Q2/src/Main.java
+++ project Q2/src/Main.java
@@ -33,8 +33,8 @@ public class Main {
         double minDis = Collections.min(distance);
         double maxDis = Collections.max(distance);
 
-        System.out.print("The centroid is "+centroidx+", "+centroidy+
-                ".\nSum of distance is "+sumDis+
+        System.out.print("The centroid is ( "+centroidx+", "+centroidy+
+                " ).\nSum of distance is "+sumDis+
                 ".\nShortest distance is "+minDis+
                 ".\nLongest distance is "+maxDis+".");
     }

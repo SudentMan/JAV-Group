de85e37abffaee0b13adfdb0da63963f53cec9f8
Tht-worada <thitibell123@gmail.com>
2026-01-20 21:45:23 +0700

    LAB 02-03 base by Bon, fine-tune by Thitiworada

---
+++ project Q3/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Point A: ");
+        Point A = new Point(sc.nextDouble(),sc.nextDouble());
+        System.out.print("Point B: ");
+        Point B = new Point(sc.nextDouble(),sc.nextDouble());
+
+        System.out.println("A = " + A.toString() + "  B = " + B.toString() +
+                "  Distance = " + A.distance(B));
+
+        System.out.print("Move the points: ");
+        double dx = sc.nextDouble();
+        double dy = sc.nextDouble();
+        A.translate(dx, dy);
+        B.translate(dx, dy);
+
+        System.out.println("A = " + A.toString() + "  B = " + B.toString() +
+                "  Distance = " + A.distance(B));
+        if (A.equals(B)) {
+            System.out.println("A and B are at the same position.");
+        } else {
+            System.out.println("A and B are not at the same position.");
+        }
+
+    }
+}
---
+++ project Q3/src/Point.java
@@ -0,0 +1,22 @@
+public class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double distance(Point other) {
+        return (Math.sqrt(Math.pow((this.x-other.x),2)+
+                Math.pow((this.y-other.y),2)));
+    }
+    public void translate(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+    public boolean equals(Point other) {
+        return (this.x == other.x && this.y == other.y);
+    }
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
